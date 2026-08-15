03177ffbc023093023ac6b573a33068cb7f1e5a8
SakshamShandilya <sakshamshandilya91@gmail.com>
2026-08-15 22:56:09 +0530

    Time: 7 ms (56.43%), Space: 12.3 MB (57.7%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def intToRoman(self, num):
-        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
 
         result = ""
 
-        for i in range(len(values)):
-            while num >= values[i]:
+        for i in range(len(val)):
+            while num >= val[i]:
                 result += symbols[i]
-                num -= values[i]
+                num -= val[i]
 
         return result
